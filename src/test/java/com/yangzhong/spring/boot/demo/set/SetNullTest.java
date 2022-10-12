1434cb262e3184c02ea54831e7c3bfa663f59ea9
杨忠 <249934888@qq.com>
2022-10-12 15:59:12 +0800

    set null test

---
+++ src/test/java/com/yangzhong/spring/boot/demo/set/SetNullTest.java
@@ -0,0 +1,18 @@
+package com.yangzhong.spring.boot.demo.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author yangzhong
+ * @Date 2022-09-14 18:30
+ * @Description 描述
+ */
+public class SetNullTest {
+
+    public static void main(String[] args) {
+        Set<Long> sets = new HashSet<>();
+        sets.add(1L);
+        System.out.println(sets.contains(null));
+    }
+}
